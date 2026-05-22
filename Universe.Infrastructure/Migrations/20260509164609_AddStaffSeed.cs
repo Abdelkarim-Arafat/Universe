6114067cd1a6e64243d720c2de65a81561e5815e
Abdelkarim <karimarafat52@gmail.com>
2026-05-23 00:05:16 +0300

    update migration

---
+++ Universe.Infrastructure/Migrations/20260509164609_AddStaffSeed.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Universe.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStaffSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Colleges_CollegeId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_CollegeId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CollegeId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MilitaryInfo_MilitaryNumber",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MilitaryInfo_DecisionNumber",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "StudentAcademicPrograms",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MilitaryInfo_MilitaryNumber",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MilitaryInfo_DecisionNumber",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CollegeId",
+                table: "Students",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "StudentAcademicPrograms",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CollegeId",
+                table: "Students",
+                column: "CollegeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Colleges_CollegeId",
+                table: "Students",
+                column: "CollegeId",
+                principalTable: "Colleges",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
